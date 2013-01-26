42f8305ce4d7ad2d5a469df45a5690beadcd6a47
Sam Neubardt <samneubardt@gmail.com>
2013-01-26 18:42:21 -0500

    Run dalmatian/-main as main func

--- project.clj
+++ project.clj
@@ -2,6 +2,7 @@
   :description "A Twitter Firehose API Multiplexer"
   :url "http://example.com/FIXME"
   :min-lein-version "2.0.0"                     
+  :main dalmatian
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
--- src/dalmatian.clj
+++ src/dalmatian.clj
@@ -1,6 +1,5 @@
 (ns dalmatian)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn -main
+  [& args]
+  )
