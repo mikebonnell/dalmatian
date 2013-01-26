1e768b7bd6e0863a269f81e624bd165dcb84545b
Sam Neubardt <samneubardt@gmail.com>
2013-01-26 18:54:22 -0500

    Pull oauth keys out of the environment

--- src/dalmatian.clj
+++ src/dalmatian.clj
@@ -4,15 +4,26 @@
             [cheshire.core :as json])
   (:import (twitter.callbacks.protocols AsyncStreamingCallback)))
 
+(defn die!
+  ([] (die! 1))
+  ([code] (System/exit code)))
+
+(defn env
+  "Retrieve the value of var-name from the system environment, or nil"
+  [var-name]
+  (System/getenv var-name))
+
+(def creds (oauth/make-oauth-creds 
+             (env "APP_CONSUMER_KEY")
+             (env "APP_CONSUMER_SECRET")
+             (env "USER_ACCESS_TOKEN")
+             (env "USER_ACCESS_TOKEN_SECRET")))
+
 (defn on-bodypart
   "Called when a new message is received from the streaming api"
   [response baos]
   (let [tweet (json/parse-string (.toString baos) true)]
     tweet))
-
-(defn die!
-  ([] (die! 1))
-  ([code] (System/exit code)))
 
 (defn on-failure
   "Called when the streaming api returns a 4xx response.
