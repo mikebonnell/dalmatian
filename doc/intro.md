c6be8872935ba0a7caacacb8e013ddecb2a49d6f
Sam Neubardt <samneubardt@gmail.com>
2013-01-26 16:53:41 -0500

    init

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to dalmatian
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ test/dalmatian/core_test.clj
@@ -0,0 +1,7 @@
+(ns dalmatian.core-test
+  (:use clojure.test
+        dalmatian.core))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
