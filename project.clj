00efa6b4d27fbd3d7169946b70caab6ae1b089a2
Sam Neubardt <samneubardt@gmail.com>
2013-01-26 18:24:46 -0500

    Add chesire and twitter-api

--- project.clj
+++ project.clj
@@ -1,6 +1,9 @@
 (defproject dalmatian "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "A Twitter Firehose API Multiplexer"
   :url "http://example.com/FIXME"
+  :min-lein-version "2.0.0"                     
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]])
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [twitter-api "0.6.10"]
+                 [cheshire "4.0.2"]])
