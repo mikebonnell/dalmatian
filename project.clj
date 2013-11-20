2e0166d6e533056e4f299f15b3b2066c83c77941
Sam Neubardt <samneubardt@gmail.com>
2013-11-20 17:33:32 -0500

    Upgrade dependencies, use environ

--- project.clj
+++ project.clj
@@ -1,11 +1,12 @@
 (defproject dalmatian "0.1.0-SNAPSHOT"
-  :description "A Twitter Firehose API Multiplexer"
+  :description "A Twitter Streaming API Multiplexer"
   :url "http://example.com/FIXME"
   :min-lein-version "2.0.0"                     
   :main dalmatian
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]
-                 [twitter-api "0.7.1"]
-                 [cheshire "5.0.1"]
-                 [clipchat "1.0.0-SNAPSHOT"]])
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [cheshire "5.2.0"]
+                 [clipchat "1.0.0-SNAPSHOT"]
+                 [environ "0.3.0"]
+                 [twitter-api "0.7.4"]])
--- src/dalmatian.clj
+++ src/dalmatian.clj
@@ -2,28 +2,24 @@
   (:require [twitter.oauth :as oauth]
             [twitter.api.streaming :as streaming]
             [cheshire.core :as json]
-            [clipchat.rooms :as hipchat])
+            [clipchat.rooms :as hipchat]
+            [environ.core :refer [env]])
   (:import (twitter.callbacks.protocols AsyncStreamingCallback)))
 
 (defn die!
   ([] (die! 1))
   ([code] (System/exit code)))
 
-(defn env
-  "Retrieve the value of var-name from the system environment, or nil"
-  ([var-name] (env var-name nil))
-  ([var-name default] (or (System/getenv var-name) default)))
-
 (def creds (oauth/make-oauth-creds 
-             (env "APP_CONSUMER_KEY")
-             (env "APP_CONSUMER_SECRET")
-             (env "USER_ACCESS_TOKEN")
-             (env "USER_ACCESS_TOKEN_SECRET")))
+             (env :app-consumer-key)
+             (env :app-consumer-secret)
+             (env :user-access-token)
+             (env :user-access-token-secret)))
 
 (def message-is-tweet? :user)
 
-(def ^:dynamic *hipchat-room-id* (env "HIPCHAT_ROOM_ID"))
-(def ^:dynamic *hipchat-auth-token* (env "HIPCHAT_AUTH_TOKEN"))
+(def hipchat-room-id (env :hipchat-room-id))
+(def hipchat-auth-token (env :hipchat-auth-token))
 
 (defn get-poster-handle-from-tweet
   [tweet]
@@ -35,20 +31,21 @@
     (let [url (str "https://twitter.com/" handle "/status/" (:id_str tweet))]
       (str "<a href=\"" url "\">" url "</a>"))))
 
+;; TODO include tweet preview
 (defn hipchat-callback
   [tweet]
   (let [permaurl (build-perma-url tweet)]
-    (println "Hipchat: sending link " permaurl)
-    (hipchat/message *hipchat-auth-token* {:room_id *hipchat-room-id* :message permaurl :from "dalmatian"})))
+    (hipchat/message hipchat-auth-token {:room_id hipchat-room-id :message permaurl :from "dalmatian"})))
 
-(def ^:dynamic *callbacks* [hipchat-callback])
-
-(defn on-bodypart
+(defn on-bodypart-fn
   "Called when a new message is received from the streaming api"
-  [response baos]
-  (let [tweet (json/parse-string (.toString baos) true)]
-    (when (message-is-tweet? tweet)
-      (hipchat-callback tweet))))
+  [callbacks]
+  (fn on-bodypart
+    [response baos]
+    (let [tweet (json/parse-string (.toString baos) true)]
+      (when (message-is-tweet? tweet)
+        (doseq [f callbacks]
+          (f tweet))))))
 
 (defn on-failure
   "Called when the streaming api returns a 4xx response.
@@ -64,20 +61,23 @@
   (println (.toString throwable))
   (die!))
 
-(def async-streaming-callback
+(defn make-async-streaming-callback
+  [callbacks]
   (AsyncStreamingCallback.
-    on-bodypart
+    (on-bodypart-fn callbacks)
     on-failure
     on-exception))
 
 (defn run!
-  [track]
+  [async-streaming-callback track]
   (streaming/statuses-filter :oauth-creds creds :callbacks async-streaming-callback :params {:track track}))
 
 (defn -main
   [& _]
-  (if-let [track (env "TRACK")]
-    (run! track)
+  (if-let [track (env :track)]
+    (let [callbacks [hipchat-callback]
+          async-streaming-callback (make-async-streaming-callback callbacks)]
+      (run! async-streaming-callback track))
     (do
       (println "FATAL: nothing defined for env var TRACK")
       (die! 3))))
